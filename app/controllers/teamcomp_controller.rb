30e7e16d429543c2456c466b1c1e934fe636bd3a
Paul <paul.m.kim321@gmail.com>
2015-11-01 14:40:36 -0800

    teamcomp backend finished

--- app/controllers/teamcomp_controller.rb
+++ app/controllers/teamcomp_controller.rb
@@ -3,16 +3,38 @@ class TeamcompController < ApplicationController
     top_champ = params[:top]
     jungle_champ = params[:jungle]
     mid_champ = params[:mid]
-    adc = params[:adc]
-    support = params[:support]
+    adc_champ = params[:adc]
+    support_champ = params[:support]
 
     #puts "DEBUG: Inside teamcomp#index!"
 
     #Do any database access codes below
+    @champion = Champion.all
     @games = Game.all
-    #    @wins = @games.where(WIN_TOP:@top.id,WIN_JG:@jg.id,WIN_MID:@mid.id,WIN_ACD:@adc.id,WIN_SUP:@sup.id).count
-    #    @losses = @games.where(LOSE_TOP:@top.id,LOSE_JG:@jg.id,LOSE_MID:@mid.id,LOSE_ACD:@adc.id,LOSE_SUP:@sup.id).count
-    @wins = 3
-    @losses = 1
+    @top_name = top_champ
+    @jungle_name = jungle_champ
+    @mid_name = mid_champ
+    @adc_name = adc_champ
+    @support_name = support_champ
+
+    @valid = false
+    @top = @champion.where(name:@top_name).pluck(:id)
+    @jungle = @champion.where(name:@jungle_name).pluck(:id)
+    @mid = @champion.where(name:@mid_name).pluck(:id)
+    @adc = @champion.where(name:@adc_name).pluck(:id)
+    @support = @champion.where(name:@support_name).pluck(:id)
+    #if(@champion.where(name:@top_name).count == 1 && @champion.where(name:@jungle_name).count == 1 && @champion.where(name:@mid_name).count == 1 && @champion.where(name:@adc_name).count == 1 && @champion.where(name:@support_name).count == 1)
+    if(!@top.blank? && !@jungle.blank? && !@mid.blank? && !@adc.blank? && !@support.blank?)
+      @wins = @games.where(WIN_TOP:@top,WIN_JG:@jungle,WIN_MID:@mid,WIN_ADC:@adc,WIN_SUP:@support).count
+      @losses = @games.where(LOSE_TOP:@top,LOSE_JG:@jungle,LOSE_MID:@mid,LOSE_ADC:@adc,LOSE_SUP:@support).count
+      @num_games = 0.0
+      @num_games = @wins + @losses
+      if(@num_games > 0)
+        @win_rate = (@wins + 0.0)/@num_games
+      else
+        @win_rate = 0
+      end
+      @valid = true
+    end
   end
 end
