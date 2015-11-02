5d31cfa44297611d8b6f601f3ad484b330be3e6a
yaodaniel <Daniel988@ucla.edu>
2015-11-01 17:30:13 -0800

    working search query for counterpicks

--- app/controllers/counterpicks_controller.rb
+++ app/controllers/counterpicks_controller.rb
@@ -1,5 +1,11 @@
 class CounterpicksController < ApplicationController
   def index
-    @blah="blah"
+    lane = params[:lane] #Lane the user searched for
+    championSearched = params[:champSearched] #Champion user wishes to find counters for
+
+    #Do any database access codes below
+
+
+
   end
 end
