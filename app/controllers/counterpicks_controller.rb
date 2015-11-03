f1edfba8700ef75d009e3f2339e712bd5f764afc
Paul <paul.m.kim321@gmail.com>
2015-11-02 18:36:39 -0800

    fixed typo in counters

--- app/controllers/counterpicks_controller.rb
+++ app/controllers/counterpicks_controller.rb
@@ -1,18 +1,22 @@
 class CounterpicksController < ApplicationController
   def index
     lane = params[:lane] #Lane the user searched for
-    championSearched = params[:champSearched] #Champion user wishes to find counters for
+    championSearched = params[:champSearch] #Champion user wishes to find counters for
 
     @lane = lane
-    @champion_name = "Aatrox"#championSearched
+    @champion_name = championSearched
     #Do any database access codes below
     @champion = Champion.all
     @counters = Counter.all
 
-    @valid = false
+    @valid = 0
     @champion_id = @champion.where(name:@champion_name).pluck(:id)
 
-    if(@lane != nil)
+    if(@lane == nil)
+      @valid = 2
+    end
+
+    if(!@champion_id.blank?)
       @counters = @counters.where(id:@champion_id)
       @first_counter = @counters.pluck(:first_counter_name)
       @wins_against_first = @counters.pluck(:wins_against_first)
@@ -23,7 +27,7 @@ class CounterpicksController < ApplicationController
       @third_counter = @counters.pluck(:third_counter_name)
       @wins_against_third = @counters.pluck(:wins_against_third)
       @losses_against_third = @counters.pluck(:losses_against_third)
-      @valid = true
+      @valid = 1
     end
 
   end
