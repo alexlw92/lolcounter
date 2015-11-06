1b8eb7889f9d0a52638fff0a764d38e876b31606
yaodaniel <Daniel988@ucla.edu>
2015-11-05 19:00:08 -0800

    removed redundant GET request tokens and linked champions page to database

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -5,17 +5,22 @@ class ChampionsController < ApplicationController
 
   def show
     @champion = Champion.find(params[:id])
-    @games = @champion.games
-    @total_games = @games.count
-    @top_win = @games.where(WIN_TOP:@champion.id).count
-    @top_loss = @total_games - @top_win
-    @jungle_win = @games.where(WIN_JG:@champion.id).count
-    @jungle_loss = @total_games - @jungle_win
-    @mid_win = @games.where(WIN_MID:@champion.id).count
-    @mid_loss = @total_games - @mid_win
-    @adc_win = @games.where(WIN_ADC:@champion.id).count
-    @adc_loss = @total_games - @adc_win
-    @support_win = @games.where(WIN_SUP:@champion.id).count
-    @support_loss = @total_games - @support_win
+    # Can definitely speed up some of these queries... Or perhaps we should store #Games played associated with a champion
+    # In the champions table
+    @total_games = Game.where('WIN_TOP = ? OR LOSE_TOP = ? OR WIN_JG = ? OR LOSE_JG = ? OR WIN_MID = ?
+                              OR LOSE_MID = ? OR WIN_ADC = ? OR LOSE_ADC = ? OR WIN_SUP = ? OR LOSE_SUP = ?',
+                              @champion.id, @champion.id, @champion.id, @champion.id, @champion.id, @champion.id,
+                              @champion.id, @champion.id, @champion.id, @champion.id).count;
+
+    @top_win = Game.where(WIN_TOP:@champion.id).count;
+    @top_loss = Game.where(LOSE_TOP:@champion.id).count;
+    @jungle_win = Game.where(WIN_JG:@champion.id).count;
+    @jungle_loss = Game.where(LOSE_JG:@champion.id).count;
+    @mid_win = Game.where(WIN_MID:@champion.id).count;
+    @mid_loss = Game.where(LOSE_MID:@champion.id).count;
+    @adc_win = Game.where(WIN_ADC:@champion.id).count;
+    @adc_loss = Game.where(LOSE_ADC:@champion.id).count;
+    @support_win = Game.where(WIN_SUP:@champion.id).count;
+    @support_loss = Game.where(LOSE_SUP:@champion.id).count;
   end
 end
