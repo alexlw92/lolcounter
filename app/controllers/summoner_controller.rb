cb0c0394767302b8a84cbd8f9ddb6f0d932aa6c0
Roland Zeng <rzeng@g.ucla.edu>
2015-11-18 01:05:04 -0800

    added summoner search page functionality

--- app/controllers/summoner_controller.rb
+++ app/controllers/summoner_controller.rb
@@ -5,50 +5,105 @@ class SummonerController < ApplicationController
     input_region = params[:input_region]
     @displayInfo = false
 
-    # VALIDATION
-
-
     # PARSE JSON
     if input_name and input_region != nil
       @displayInfo = true
 
+      input_name = input_name.gsub(/\s+/, "")
+
       result = RiotApiModel.summoner_lookup(input_name,input_region)
-      @output_summoner_name = result[input_name]['name']
-      @output_summoner_region = input_region.upcase
-      summoner_level = result[input_name]['summonerLevel']
+      if result.is_a?Integer
+        @valid_sum_name = "nope"
+      else
+        @valid_sum_name = 'valid'
+        @output_summoner_name = result[input_name]['name']
+        @output_summoner_region = input_region.upcase
+        summoner_level = result[input_name]['summonerLevel']
+        summoner_icon = result[input_name]['profileIconId']
+        @output_summoner_icon = 'http://ddragon.leagueoflegends.com/cdn/5.22.3/img/profileicon/' + summoner_icon.to_s + '.png'
 
-      summoner_icon = result[input_name]['profileIconId']
-      @output_summoner_icon = 'http://ddragon.leagueoflegends.com/cdn/5.20.1/img/profileicon/' + summoner_icon.to_s + '.png'
-      p @output_summoner_icon
-      summoner_id = result[input_name]['id']
+        if summoner_level < 30
+          @output_summoner_rank = 'Level ' + summoner_level.to_s
+          @valid_rank = 'no'
 
-      result2 = RiotApiModel.rank_lookup(summoner_id, input_region)
+        else
+          summoner_id = result[input_name]['id']
 
-      summoner_tier = result2[summoner_id.to_s][0]['tier']
-      summoner_division = result2[summoner_id.to_s][0]['entries'][0]['division']
-      summoner_lp =  result2[summoner_id.to_s][0]['entries'][0]['leaguePoints']
+          result2 = RiotApiModel.rank_lookup(summoner_id, input_region)
 
-      puts('------------------')
-      p summoner_icon
-      p summoner_tier
-      p summoner_division
-      p summoner_lp
+          if result2.is_a?Integer
+            @output_summoner_rank = 'No solo queue ranked data found!'
+          else
 
-      @output_summoner_tier = summoner_tier
-      @output_summoner_divison = summoner_division
-      @output_summoner_lp = summoner_lp
+            if result2[summoner_id.to_s][0]['queue'].to_s != 'RANKED_SOLO_5x5'
+              @valid_rank = 'no'
+              @output_summoner_rank = 'No solo queue ranked data found!'
+            else
+              @valid_rank = 'valid'
+              summoner_tier = result2[summoner_id.to_s][0]['tier']
+              summoner_division = result2[summoner_id.to_s][0]['entries'][0]['division']
+              summoner_lp =  result2[summoner_id.to_s][0]['entries'][0]['leaguePoints']
+              @output_summoner_rank = summoner_tier.to_s + ' ' + summoner_division.to_s + ' ' + summoner_lp.to_s + 'LP'
 
-      result3 = RiotApiModel.get_ranked_summary(summoner_id)
-      arr = result3['champions']
+              result3 = RiotApiModel.get_ranked_summary(summoner_id)
+              arr = result3['champions']
 
-      #Sorts arr in increasing order
-      #Last element is total stats
-      #2nd last is most played
-      #3rd last is 2nd most played
-      #4th last is the 3rd most played
+              #Sorts arr in increasing order
+              #Last element is total stats
+              #2nd last is most played
+              #3rd last is 2nd most played
+              #4th last is the 3rd most played
 
-      arr.sort! {|a,b| a['stats']['totalSessionsPlayed'] <=> b['stats']['totalSessionsPlayed'] }
-        p arr[0]['stats']['totalSessionsPlayed']
+              arr.sort! {|a,b| a['stats']['totalSessionsPlayed'] <=> b['stats']['totalSessionsPlayed'] }
+
+              top1_num = arr[arr.length-2]['stats']['totalSessionsPlayed']
+              top1_id = arr[arr.length-2]['id']
+              top2_num = arr[arr.length-3]['stats']['totalSessionsPlayed']
+              top2_id = arr[arr.length-3]['id']
+              top3_num = arr[arr.length-4]['stats']['totalSessionsPlayed']
+              top3_id = arr[arr.length-4]['id']
+
+              top1_wins = arr[arr.length-2]['stats']['totalSessionsWon']
+              top2_wins = arr[arr.length-3]['stats']['totalSessionsWon']
+              top3_wins = arr[arr.length-4]['stats']['totalSessionsWon']
+
+
+              top1_champ = RiotApiModel.get_champion_by_id(top1_id)['name']
+              top2_champ = RiotApiModel.get_champion_by_id(top2_id)['name']
+              top3_champ = RiotApiModel.get_champion_by_id(top3_id)['name']
+
+              @output_top1_icon = 'http://ddragon.leagueoflegends.com/cdn/5.22.3/img/champion/' + top1_champ.to_s + '.png'
+              @output_top2_icon = 'http://ddragon.leagueoflegends.com/cdn/5.22.3/img/champion/' + top2_champ.to_s + '.png'
+              @output_top3_icon = 'http://ddragon.leagueoflegends.com/cdn/5.22.3/img/champion/' + top3_champ.to_s + '.png'
+
+              @output_top1_champ = top1_champ
+              @output_top2_champ = top2_champ
+              @output_top3_champ = top3_champ
+
+              @output_top1_games = top1_num
+              @output_top2_games = top2_num
+              @output_top3_games = top3_num
+
+              @output_top1_wr = ((top1_wins+0.0)*100/ top1_num).to_i
+              @output_top2_wr = ((top2_wins+0.0)*100/ top2_num).to_i
+              @output_top3_wr = ((top3_wins+0.0)*100/ top3_num).to_i
+
+              @output_top1_k = ((arr[arr.length-2]['stats']['totalChampionKills'] +0.0) / top1_num).round(1)
+              @output_top2_k = ((arr[arr.length-3]['stats']['totalChampionKills'] +0.0) / top2_num).round(1)
+              @output_top3_k = ((arr[arr.length-4]['stats']['totalChampionKills'] +0.0) / top3_num).round(1)
+
+              @output_top1_d = ((arr[arr.length-2]['stats']['totalDeathsPerSession'] +0.0) / top1_num).round(1)
+              @output_top2_d = ((arr[arr.length-3]['stats']['totalDeathsPerSession'] +0.0) / top2_num).round(1)
+              @output_top3_d = ((arr[arr.length-4]['stats']['totalDeathsPerSession'] +0.0) / top3_num).round(1)
+
+              @output_top1_a = ((arr[arr.length-2]['stats']['totalAssists'] +0.0) / top1_num).round(1)
+              @output_top2_a = ((arr[arr.length-3]['stats']['totalAssists'] +0.0) / top2_num).round(1)
+              @output_top3_a = ((arr[arr.length-4]['stats']['totalAssists'] +0.0) / top3_num).round(1)
+
+            end
+          end
+        end
+      end
     end
   end
 
--- app/models/riot_api_model.rb
+++ app/models/riot_api_model.rb
@@ -5,25 +5,48 @@ class RiotApiModel < ActiveRecord::Base
 
   def self.summoner_lookup(sum_name,sum_region)
     url ="https://#{sum_region}.api.pvp.net/api/lol/#{sum_region}/v1.4/summoner/by-name/#{sum_name}?api_key=80f2a95e-b7f8-44d0-894b-b02201b98dc0"
-    result = RestClient.get(url, :accept => 'json')
-    return JSON.parse(result)
+    #result = RestClient.get(url, :accept => 'json')
+
+    RestClient.get(url){ |response, request, result, &block|
+      case response.code
+        when 200
+          return JSON.parse(response)
+        else
+          return response.code
+      end
+    }
+
   end
 
   def self.rank_lookup(sum_id, sum_region)
     url = "https://#{sum_region}.api.pvp.net/api/lol/#{sum_region}/v2.5/league/by-summoner/#{sum_id}/entry?api_key=80f2a95e-b7f8-44d0-894b-b02201b98dc0"
-    result = RestClient.get(url, :accept => 'json')
-    return JSON.parse(result)
+    #result = RestClient.get(url, :accept => 'json')
+
+    RestClient.get(url){ |response, request, result, &block|
+      case response.code
+        when 200
+          return JSON.parse(response)
+        else
+          return response.code
+      end
+    }
 
   end
 
   def self.get_champions()
-    url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=3a5fa3f0-c714-4a0a-9dab-955dcdc04bca"
+    url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=80f2a95e-b7f8-44d0-894b-b02201b98dc0"
     result = RestClient.get(url, :accept => 'json')
     return JSON.parse(result)
   end
 
   def self.get_ranked_summary(sum_id)
-    url = "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/#{sum_id}/ranked?season=SEASON2015&api_key=3a5fa3f0-c714-4a0a-9dab-955dcdc04bca"
+    url = "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/#{sum_id}/ranked?season=SEASON2015&api_key=80f2a95e-b7f8-44d0-894b-b02201b98dc0"
+    result = RestClient.get(url, :accept => 'json')
+    return JSON.parse(result)
+  end
+
+  def self.get_champion_by_id(champ_id)
+    url = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/#{champ_id}?api_key=80f2a95e-b7f8-44d0-894b-b02201b98dc0"
     result = RestClient.get(url, :accept => 'json')
     return JSON.parse(result)
   end
