866024fb1590527a0e57ae8b6f1ba2d336d3cb85
Roland Zeng <rzeng@g.ucla.edu>
2015-11-24 19:56:50 -0800

    part 1 of counterpick table

--- app/controllers/counterpicks_controller.rb
+++ app/controllers/counterpicks_controller.rb
@@ -4,6 +4,7 @@ class CounterpicksController < ApplicationController
     championSearched = params[:champSearch] #Champion user wishes to find counters for
 
     @lane = lane
+    @output_lane = lane.to_s.capitalize
     @champion_name = championSearched
     #Do any database access codes below
     @champion = Champion.all
@@ -18,15 +19,31 @@ class CounterpicksController < ApplicationController
 
     if(!@champion_id.blank?)
       @counters = @counters.where(id:@champion_id)
+
       @first_counter = @counters.pluck(:first_counter_name)
-      @wins_against_first = @counters.pluck(:wins_against_first)
-      @losses_against_first = @counters.pluck(:losses_against_first)
+      @first_icon = 'http://ddragon.leagueoflegends.com/cdn/5.22.3/img/champion/' + @first_counter.to_s + '.png'
+
+      wins_against_first = @counters.pluck(:wins_against_first)
+      losses_against_first = @counters.pluck(:losses_against_first)
+      @first_games = wins_against_first + losses_against_first
+      #@first_wr = ((wins_against_first+0.0)*100/ @first_games).to_i
+
       @second_counter = @counters.pluck(:second_counter_name)
-      @wins_against_second = @counters.pluck(:wins_against_second)
-      @losses_against_second = @counters.pluck(:losses_against_second)
+      @second_icon = 'http://ddragon.leagueoflegends.com/cdn/5.22.3/img/champion/' + @second_counter.to_s + '.png'
+
+      wins_against_second = @counters.pluck(:wins_against_second)
+      losses_against_second = @counters.pluck(:losses_against_second)
+      @second_games = wins_against_second + losses_against_second
+      #@second_wr = ((wins_against_second+0.0)*100/ @second_games).to_i
+
       @third_counter = @counters.pluck(:third_counter_name)
-      @wins_against_third = @counters.pluck(:wins_against_third)
-      @losses_against_third = @counters.pluck(:losses_against_third)
+      @third_icon = 'http://ddragon.leagueoflegends.com/cdn/5.22.3/img/champion/' + @third_counter.to_s + '.png'
+
+      wins_against_third = @counters.pluck(:wins_against_third)
+      losses_against_third = @counters.pluck(:losses_against_third)
+      @third_games = wins_against_third + losses_against_third
+      #@third_wr = ((wins_against_third+0.0)*100/ @third_games).to_i
+
       @valid = 1
     end
 
