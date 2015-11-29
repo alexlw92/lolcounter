dd4978798b7d45ef29dc8474116d280619c2d3a6
Alex Wang <alexlw92@yahoo.com>
2015-11-28 16:48:04 -0800

    syncing database pop files

--- db/alex_dbstuff/pop_db.rb
+++ db/alex_dbstuff/pop_db.rb
@@ -4,9 +4,11 @@ require "json"
 require "net/http"
 require "uri"
 
+apikey = "1b610544-8c97-4f40-adf6-37df27c37fb8"
+
 ActiveRecord::Base.establish_connection(
     :adapter  => 'mysql2',
-    :database => 'lolcount',
+    :database => 'newdatabase',
     :username => 'root',
     :password => 'root',
     :host     => 'localhost')
@@ -14,21 +16,112 @@ ActiveRecord::Base.establish_connection(
 class Summoners < ActiveRecord::Base
 end
 
-begin
-ensure
+class Games < ActiveRecord::Base
 end
-uri = URI.parse("https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/21589096?rankedQueues=RANKED_SOLO_5x5&api_key=3a5fa3f0-c714-4a0a-9dab-955dcdc04bca")
 
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
+def callAPI(uri_orig)
+  uri = URI.parse(uri_orig)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Get.new(uri.request_uri)
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  begin
+    response = http.request(request)
+  rescue
+    sleep(5)
+    return callAPI(uri_orig)
+  end
 
-request = Net::HTTP::Get.new(uri.request_uri)
-http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-response = http.request(request)
-if response.code == "200"
-  result = JSON.parse(response.body)
-else
+  if response.code == "200"
+    result = JSON.parse(response.body)
+    return result
+  elsif response.code == "429"
+    puts "RATE LIMIT"
+    puts response.code
+    time = response['retry-after']
+    if time != nil
+      sleep(Integer(time))
+    else
+      sleep(2)
+    end
+    return callAPI(uri_orig)
+  end
 end
-puts result
-puts result["matches"]
 
+
+s = Summoners.order('created_at ASC')
+
+s.each{|ss|
+
+  summoner = ss.id
+  puts "getting summoner #{summoner}"
+  result = callAPI("https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/#{summoner}?rankedQueues=RANKED_SOLO_5x5&api_key=#{apikey}")
+  sleep(0.01)
+  matches = result["matches"].first(10)
+
+
+  matches.each{ |match|
+    matchid = match["matchId"]
+    matchinfo = callAPI("https://na.api.pvp.net/api/lol/na/v2.2/match/#{matchid}?api_key=#{apikey}")
+
+    matchrecord = Games.find_or_create_by(id: matchid)
+    for i in 0..9
+      begin
+
+        summId = matchinfo["participantIdentities"][i]["player"]["summonerId"]
+        sum = Summoners.find_or_create_by(id: summId)
+        puts "added summoner #{summId}"
+
+        time = matchinfo["matchCreation"]
+        dt = Time.at(time/1000)
+        matchrecord.update(game_started_at: dt)
+
+
+        lane = matchinfo["participants"][i]["timeline"]["lane"]
+        role = matchinfo["participants"][i]["timeline"]["role"]
+        winner = matchinfo["participants"][i]["stats"]["winner"]
+        champ = matchinfo["participants"][i]["championId"]
+        if winner
+          if lane == "TOP"
+            matchrecord.update(WIN_TOP: champ)
+          elsif lane == "MIDDLE"
+            matchrecord.update(WIN_MID: champ)
+          elsif lane == "JUNGLE"
+            matchrecord.update(WIN_JG: champ)
+          elsif lane == "BOTTOM"
+            if role == "DUO_CARRY"
+              matchrecord.update(WIN_ADC: champ)
+            elsif role == "DUO_SUPPORT"
+              matchrecord.update(WIN_SUP: champ)
+            end
+          end
+        else
+          if lane == "TOP"
+            matchrecord.update(LOSE_TOP: champ)
+          elsif lane == "MIDDLE"
+            matchrecord.update(LOSE_MID: champ)
+          elsif lane == "JUNGLE"
+            matchrecord.update(LOSE_JG: champ)
+          elsif lane == "BOTTOM"
+            if role == "DUO_CARRY"
+              matchrecord.update(LOSE_ADC: champ)
+            elsif role == "DUO_SUPPORT"
+              matchrecord.update(LOSE_SUP: champ)
+            end
+          end
+        end
+
+      rescue
+      ensure
+      end
+
+    end
+
+    puts "added match #{matchid}"
+
+    sleep(1)
+
+
+  }
+  ss.destroy
+}
